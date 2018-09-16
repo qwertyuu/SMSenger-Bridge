4ea8977ccd79c556d1fc4512e5d90e9d4d13a32d
Raphaël Côté <cotlarrc@msn.com>
2018-09-16 14:45:00 -0400

    Added fuzzy search in current messenger thread
    Removes old facebook search function

--- smsListener.py
+++ smsListener.py
@@ -1,5 +1,6 @@
 from fbchat import Message
 from flask import request
+from fuzzywuzzy import process
 import re
 
 
@@ -13,12 +14,16 @@ class SMSListener:
         body = request.args.get('Body')
         if body:
             text_search = re.search(r"(?:^@([^:]+)\:\s?)?(.*)", body)
-            if text_search.group(1) is not None:
-                search_result = self.messenger.searchForUsers(text_search.group(1))
-                if len(search_result):
-                    first_result = search_result[0]
-                    self.recipient = first_result.uid
+            user_search_term = text_search.group(1)
             text_to_send = text_search.group(2)
+
+            if user_search_term is not None:
+                users = {user.uid: user.name for user in self.messenger.fetchAllUsers()}
+                search_result = process.extractOne(user_search_term, users)
+                user_id = search_result[2]
+                if user_id:
+                    self.recipient = user_id
+
             if self.recipient and text_to_send:
                 self.messenger.send(Message(text=text_to_send), thread_id=self.recipient)
         return "nothing to see here"
